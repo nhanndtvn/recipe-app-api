d6a312d820387d7e80d327a79f785dacae8b8389
nhanndtvn <thunhann@yahoo.com>
2024-01-12 14:51:59 +0700

    Add new

--- app/app/tests.py
+++ app/app/tests.py
@@ -12,13 +12,11 @@ class CalcTest(SimpleTestCase):
     def test_add_number(self):
         """Test adding number"""
         # 5. Setup input + 6. Execute
-        res = calc.add(5, 6)                
+        res = calc.add(5, 6)
         # 7. Check output
         self.assertEqual(res, 11)
-    
+
     # USING TDD
     def test_substract_number(self):
         res = calc.subtract(10, 15)  # subtract function is not defined
-        
-        self.assertEqual(res, 5) 
-        
+        self.assertEqual(res, 5)
